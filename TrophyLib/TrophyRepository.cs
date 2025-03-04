f520bf721b676b3bfeb97b37a0269a2fb0edecf9
Morten Chami Berner <mob001@edu.zealand.dk>
2025-03-04 16:59:35 +0100

    Added TrophyRepository class

---
+++ TrophyLib/TrophyRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrophyLib
+{
+    public class TrophyRepository
+    {
+        private List<Trophy> trophies = new List<Trophy>();
+        public void Add(Trophy trophy)
+        {
+            if (trophies.Contains(trophy))
+                throw new ArgumentException("Trophy already exists");
+            trophies.Add(trophy);
+        }
+        public void Remove(Trophy trophy)
+        {
+            if (!trophies.Contains(trophy))
+                throw new ArgumentException("Trophy does not exist");
+            trophies.Remove(trophy);
+        }
+        public List<Trophy> GetAll()
+        {
+            return trophies;
+        }
+        public Trophy GetById(int id)
+        {
+            return trophies.Find(t => t.Id == id);
+        }
+        public List<Trophy> GetByCompetition(string competition)
+        {
+            return trophies.FindAll(t => t.Competition == competition);
+        }
+        public List<Trophy> GetByYear(int year)
+        {
+            return trophies.FindAll(t => t.Year == year);
+        }
+        public List<Trophy> GetByYearRange(int startYear, int endYear)
+        {
+            return trophies.FindAll(t => t.Year >= startYear && t.Year <= endYear);
+        }
+    }
+}
---
+++ TrophyLibTests/TrophyRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrophyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrophyLib.Tests
+{
+    [TestClass()]
+    public class TrophyRepositoryTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByCompetitionTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByYearTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByYearRangeTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
